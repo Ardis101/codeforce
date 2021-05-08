0e5382646549598b9e80541d0d60880c5022d0b4
ardis <ardis@DESKTOP-7986Q2K>
2021-05-08 20:53:47 +0100

    Domino Piling

---
+++ testproject/src/DominoPiling.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DominoPiling {
+
+	public static void main(String[] args) {
+		
+		int DOMINO = 2;
+		
+        Scanner sc = new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        
+        int mIncluds = m/DOMINO;
+        int first = n * mIncluds;
+        
+        if(m%2 == 0) {
+        	System.out.println(first);
+        }else {
+        	System.out.println(n/DOMINO + first);
+        }
+
+	}
+
+}
