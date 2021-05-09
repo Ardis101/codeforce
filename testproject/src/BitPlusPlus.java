0d53af91d5be5c90a43332ff3bd60bdbc6349a57
ardis <ardis@DESKTOP-7986Q2K>
2021-05-09 17:22:54 +0100

    done

--- testproject/src/BitPlusPlus.java
+++ testproject/src/BitPlusPlus.java
@@ -5,13 +5,23 @@ public class BitPlusPlus {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
         int testCases=sc.nextInt();
+        int x = 0;
         
         for (int i=0; i<(testCases+1); i++){
             String n=sc.nextLine();
             
-            
+            if (n.equals("X++")) {
+            	x++;
+            }else if(n.equals("--X")) {
+            	x--;
+            }else if(n.equals("++X")){
+            	x++;
+            }else if(n.equals("X--")) {
+            	x--;
+            }
             	
         }
+        System.out.println(x);
          sc.close();   
 	}
 
